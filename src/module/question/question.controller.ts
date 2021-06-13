2512597dfbdfa965aabbc2ab4373b4cc1a7a5a02
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-13 12:39:51 -0300

    created routes do recover questions

--- src/module/question/question.controller.ts
+++ src/module/question/question.controller.ts
@@ -2,14 +2,20 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { CreateQuestionDTO } from './dtos/CreateQuestion.dto';
+import { RecoverQuestionsDTO } from './dtos/RecoverQuestions.dto';
 import { QuestionService } from './question.service';
 
 @Controller()
 export class QuestionController {
-  constructor(private readonly questionController: QuestionService) {}
+  constructor(private readonly questionService: QuestionService) {}
 
   @MessagePattern('createQuestion')
   async create(@Payload() data: CreateQuestionDTO) {
-    return this.questionController.create(data);
+    return this.questionService.create(data);
+  }
+
+  @MessagePattern('recoverQuestions')
+  async recoverQuestions(@Payload() data: RecoverQuestionsDTO) {
+    return this.questionService.recoverQuestions(data);
   }
 }
