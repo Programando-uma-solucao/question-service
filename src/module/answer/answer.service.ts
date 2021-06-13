ca0d8c5702bc6f5546a13b34387e8e0c631505f6
Hermes Francisco <franciscohermes2000@hotmail.com>
2021-06-13 16:17:50 -0300

    updateOne para mudar o hasResponse da pergunta

--- src/module/answer/answer.service.ts
+++ src/module/answer/answer.service.ts
@@ -3,6 +3,7 @@ import { ClientProxy } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CipherServiceConfig } from 'src/config/microservices.config';
+import { Question, QuestionDocument } from '../question/schemas/question.schema';
 import { CreateAnswerDTO } from './dtos/CreateAnswer.dto';
 import { Answer, AnswerDocument } from './schemas/answer.schema';
 
@@ -10,6 +11,7 @@ import { Answer, AnswerDocument } from './schemas/answer.schema';
 export class AnswerService {
   constructor(
     @InjectModel(Answer.name) private answerModel: Model<AnswerDocument>,
+    @InjectModel(Question.name) private questionModel: Model<QuestionDocument>,
     @Inject(CipherServiceConfig.name)
     private readonly cipherService: ClientProxy,
   ) {}
@@ -26,6 +28,7 @@ export class AnswerService {
     };
 
     const createdAnswer = new this.answerModel(dataToSave);
+    await this.questionModel.updateOne({ _id : data.questionId },{ hasResponse : true });
     const savedAnswer = await createdAnswer.save();
 
     return savedAnswer;
